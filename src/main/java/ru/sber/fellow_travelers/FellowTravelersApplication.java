c3d102d53db72ab41e029f4ebd8a36ab75aa4d8e
Maxtarus <m4xstarstarostin@yandex.ru>
2024-03-03 11:09:51 +0300

    Initial commit

---
+++ src/main/java/ru/sber/fellow_travelers/FellowTravelersApplication.java
@@ -0,0 +1,13 @@
+package ru.sber.fellow_travelers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FellowTravelersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FellowTravelersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/sber/fellow_travelers/FellowTravelersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sber.fellow_travelers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FellowTravelersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
